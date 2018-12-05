14c57fcf572e8cc97202f11392acc2adb655cd9d
wang <wws19125@126.com>
2018-12-05 16:24:27 +0800

    kill adblock warning

--- killcsdn/js/kill.js
+++ killcsdn/js/kill.js
@@ -4,10 +4,20 @@ if(targetBtn)
 {
     targetBtn.click();
 }
-(function killAdblock(){
-    var target = document.querySelector(".adblock");
+function killAdblock(selector){
+    var target = document.querySelector(selector);
     if(target)
     {
         target.remove();
     }
-})();
+}
+//2018年12月5日出现强制要求加入AdBlock白名单,干掉
+(function killAdblockWarning(){
+    var it = window.setInterval(";");
+    for(var i=0;i<it;i++)
+    {
+        window.clearInterval(i);
+    }
+    killAdblock(".check-adblock-bg");
+    killAdblock(".adblock");
+})()
